63d1c645546abd6dee337911f917d4474e520f63
Jesse <jesse.lare@gmail.com>
2023-12-22 23:29:05 -0500

    updating roomlist and package

--- client/src/components/RoomList.js
+++ client/src/components/RoomList.js
@@ -1,33 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import { getRooms } from '../services/api';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
 
 const RoomList = () => {
     const [rooms, setRooms] = useState([]);
+    const [selectedRoom, setSelectedRoom] = useState(null);
+    const [openDialog, setOpenDialog] = useState(false);
 
     useEffect(() => {
-        const fetchRooms = async () => {
-            try {
-                const response = await getRooms();
-                setRooms(response.data); // Make sure to use response.data to get the actual rooms array
-            } catch (error) {
+        // Replace with your actual API call
+        axios.get('http://localhost:5000/api/rooms')
+            .then((response) => {
+                setRooms(response.data);
+            })
+            .catch((error) => {
                 console.error('Error fetching rooms:', error);
-                // Handle error here, for example by setting an error state and displaying a message
-            }
-        };
-
-        fetchRooms();
+            });
     }, []);
+
+    const handleOpenDialog = (room) => {
+        setSelectedRoom(room);
+        setOpenDialog(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
 
     return (
         <div>
-            <h1>Room List</h1>
-            <ul>
-                {rooms.map((room) => (
-                    <li key={room._id}>
-                        {room.roomNumber} - {room.roomType} - {room.roomStatus}
-                    </li>
-                ))}
-            </ul>
+            <Button variant="contained" onClick={() => setOpenDialog(true)}>
+                Room List
+            </Button>
+            <Dialog open={openDialog} onClose={handleCloseDialog}>
+                <DialogTitle>Rooms</DialogTitle>
+                <DialogContent>
+                    <List>
+                        {rooms.map((room) => (
+                            <ListItem button key={room._id} onClick={() => handleOpenDialog(room)}>
+                                <ListItemText primary={`Room Number: ${room.roomNumber}`} />
+                            </ListItem>
+                        ))}
+                    </List>
+                    {selectedRoom && (
+                        <DialogContentText>
+                            <strong>Room Name:</strong> {selectedRoom.roomName}<br />
+                            <strong>Devices:</strong> {/* Map through devices here */}<br />
+                            <strong>Open Issues:</strong> {/* Map through open issues here */}<br />
+                            <strong>Resolved Issues:</strong> {/* Map through resolved issues here */}
+                        </DialogContentText>
+                    )}
+                </DialogContent>
+            </Dialog>
         </div>
     );
 };
